04f88f695ffcbdddafc022cf682fc09f193d8e62
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-06-30 07:30:00 +0600

    Added Splash

---
+++ app/src/main/java/com/example/hotelmanagementsystem/Splash.java
@@ -0,0 +1,25 @@
+package com.example.hotelmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        }, 4000);
+    }
+}
